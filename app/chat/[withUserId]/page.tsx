8efb7cf9987335ff2e92a38b43332b816df0668e
Chinmay Billava <141098334+chinmay-b05@users.noreply.github.com>
2025-04-24 01:03:33 +0530

    Update page.tsx 6

--- app/chat/[withUserId]/page.tsx
+++ app/chat/[withUserId]/page.tsx
@@ -2,8 +2,15 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Suspense } from "react";
 import ChatDataWrapper from "./components/ChatDataWrapper";
 import WithUserHeader from "./components/WithUserHeader";
+import { type NextPage } from "next";
 
-export default function Page({ params }: { params: { withUserId: string } }) {
+type PageProps = {
+  params: {
+    withUserId: string;
+  };
+};
+
+const Page = ({ params }: PageProps) => {
   return (
     <div className="flex flex-col grow w-full">
       <Suspense fallback={<Skeleton className="h-[45px] w-full" />}>
@@ -12,5 +19,7 @@ export default function Page({ params }: { params: { withUserId: string } }) {
       <ChatDataWrapper toUserId={params.withUserId} />
     </div>
   );
-}
+};
+
+export default Page;
 
