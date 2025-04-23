c860de31ea6b219523a51d729d5aecc0a0f99de3
Chinmay Billava <141098334+chinmay-b05@users.noreply.github.com>
2025-04-24 01:56:54 +0530

    Update page.tsxuser2

--- app/chat/[withUserId]/page.tsx
+++ app/chat/[withUserId]/page.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Suspense } from "react";
 import ChatDataWrapper from "./components/ChatDataWrapper";
 import WithUserHeader from "./components/WithUserHeader";
-
+import type { PageProps } from "@/types/PageProps";
 interface Params {
   withUserId: string;
 }
